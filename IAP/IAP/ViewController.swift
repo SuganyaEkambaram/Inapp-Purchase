4a13699b1d81d1f2986b6e7af81d578afdadeb47
Suganya <suganyaa.3010@gmail.com>
2018-08-08 21:07:16 +0530

    Add files via upload

---
+++ IAP/IAP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IAP
+//
+//  Created by SUGAN_IOS_DEVELOPER on 6/8/18.
+//  Copyright © 2018 MACBOOK PRO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
